3e25f2e14211b9a137da05d106ad294bd691f388
Shubhendu <shubhendu.saurabh@hotmail.com>
2014-05-10 00:32:41 +0530

    moved secret key to env variable

--- top10songs/settings.py
+++ top10songs/settings.py
@@ -17,7 +17,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y*rs*gmodn)b^b(&g=384a5v0uc^o6%%2ooi$=gi+p0%u@d3+b'
+from django.core.exceptions import ImproperlyConfigured
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s env variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -131,9 +140,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../static'),
 )
-
-# try to load local settings if it exists
-try:
-    from local_settings import *
-except Exception as e:
-    pass
