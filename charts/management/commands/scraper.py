aa9c63d62860f9e384ae2c78be693fea0d975136
Shubhendu <shubhendu.saurabh@hotmail.com>
2014-09-14 00:03:48 +0530

    updated scrapers

--- charts/management/commands/scraper.py
+++ charts/management/commands/scraper.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python
-#import os
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "top10songs.settings")
 from urllib2 import urlopen
 from urllib import urlencode
 import datetime
 import json
 from bs4 import BeautifulSoup
-from charts.models import Chart, Song
+from charts.models import Chart, Song, Ranking
 from .billboard import fetchEntries
 
 hindiURL = 'http://www.radiomirchi.com/more/mirchi-top-20/'
@@ -51,29 +48,28 @@ def scrap_eng_songs():
 def scrap_eng_songs_new():
     songs = fetchEntries()
     c = init_chart('EN')
+    i=1
     for song in songs:
-        s = Song()
-        s.name = song.name
-        s.artist = song.artist
-        s.album = song.album
-        s.youtube_id = get_youtube_id(s.name)
-        s.chart = c
-        s.save()
+        s, created = Song.objects.get_or_create(name=song.name, album=song.album, artist=song.artist)
+        if created:
+            s.youtube_id = get_youtube_id(s.name)
+            s.save()
+        Ranking.objects.create(song=s, chart=c, position=i)
+        i+=1
 
 
 def scrap_hindi_songs():
     doc = make_soup(hindiURL)
     songs = doc.findAll('div', 'header')
     c = init_chart("HI")
+    ranking = Ranking()
     for i, song in enumerate(songs):
         if i < 10:
-            s = Song()
-            s.name = song.contents[1].text.strip()
-            s.album = song.contents[3].next.strip()
-            s.artist = song.contents[3].next.next.next.strip()
-            s.youtube_id = get_youtube_id(s.name)
-            s.chart = c
-            s.save()
-
-# if __name__ == '__main__':
-#    scrap_hindi_songs()
+            name = song.contents[1].text.strip()
+            album = song.contents[3].next.strip()
+            artist = song.contents[3].next.next.next.strip()
+            s, created = Song.objects.get_or_create(name=name, album=album, artist=artist)
+            if created:
+                s.youtube_id = get_youtube_id(s.name)
+                s.save()
+            Ranking.objects.create(song=s, chart=c, position=i+1)
