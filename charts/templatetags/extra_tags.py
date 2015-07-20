1c6742233eaf2fd4d7074f34a86360b056e5c399
Shubhendu <shubhendu.saurabh@hotmail.com>
2015-07-20 20:43:41 +0530

    added navs for custom charts

--- charts/templatetags/extra_tags.py
+++ charts/templatetags/extra_tags.py
@@ -1,9 +1,16 @@
 from django import template
 from django.contrib.flatpages.models import FlatPage
 
+from charts.models import CustomChart
+
 register = template.Library()
 
 @register.inclusion_tag("footer.html")
 def footer_links():
     flatpages = FlatPage.objects.all()
     return { 'flatpages': flatpages }
+
+@register.inclusion_tag("header.html")
+def header_links():
+    custom_charts = CustomChart.objects.all()
+    return { 'custom_charts': custom_charts }
