e16fb17940a1f2782cb2d714dd34aceb186bc40f
Shubhendu Saurabh <shubhendu.saurabh@hotmail.com>
2016-04-02 22:57:16 +0530

    reverted prod settings for database

--- top10songs/settings.py
+++ top10songs/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'top10songs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        #'PASSWORD': os.environ['DB_PASS'],
-        'HOST': os.environ['DB_SERVICE'],
-        'PORT': os.environ['DB_PORT']
+        'NAME': 'top10songs',
+        'USER': 'shubhu',
+        'PASSWORD': 'shubhu',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
