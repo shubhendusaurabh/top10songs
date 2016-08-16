d897b3158c4898eab1800ad3dbe922c868610d43
Shubhendu Saurabh <shubhendu.saurabh@hotmail.com>
2016-08-17 00:00:50 +0530

    robots and meta tags and heading optimizations

--- top10songs/urls.py
+++ top10songs/urls.py
@@ -1,28 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
-# from rest_framework import routers, serializers, viewsets
+from django.http import HttpResponse
 
 from charts.sitemaps import ChartSitemap, SongSitemap, CustomChartSitemap
 admin.autodiscover()
 
-def acme(request, acme_token):
-    import os
-    from django.http import HttpResponse, Http404
-    acme_key = ''
-    if ('ACME_KEY' in os.environ and 'ACME_TOKEN' in os.environ):
-        if acme_token == os.environ['ACME_TOKEN']:
-            acme_key = os.environ['ACME_KEY']
-
-    for key in os.environ:
-        if key.startswith('ACME_TOKEN_'):
-            num = key.split('ACME_TOKEN_')[1]
-            if acme_token == os.environ['ACME_TOKEN_' + num]:
-                acme_key = os.environ['ACME_KEY_' + num]
-    if acme_key:
-        return HttpResponse(acme_key)
-    else:
-        raise Http404
 
 sitemaps = {
     'chart': ChartSitemap,
@@ -30,7 +13,7 @@ sitemaps = {
     'custom': CustomChartSitemap
 }
 urlpatterns = patterns('',
-                       url(r'^.well-known/acme-challenge/(?P<acme_token>[\w\-]+)$', acme, name='acme-challenge'),
+                       url(r'^robots.txt$', lambda r: HttpResponse("User-agent: * \nDisallow: ", content_type="text/plain"))
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
                        url(r'^search/', include('search.urls')),
